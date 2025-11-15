09e96eea3c89debdb3b0eba03b27821b4ae06c13
me <motobep@yandex.ru>
2025-11-15 16:50:57 +0300

    [Add]: doc for pluginInterfaces

--- src/MpApi/pluginInterfaces.ts
+++ src/MpApi/pluginInterfaces.ts
@@ -4,26 +4,69 @@ import { GroupItem, IndexedItem, ItemAction, MusicItem } from './types'
  * Must be implemented by source plugins ("type": "js:source")
  */
 export interface SourcePlugin {
+    /**
+     * Called after this plugin was loaded
+     */
     afterInitAsync(): Promise<void>
+    /**
+     * Called when user reloads page
+     */
     reloadAsync(): Promise<void>
 
+    /**
+     * On choose this plugin's source
+     */
     chooseSourceAsync(): Promise<void>
 
+    /**
+     * Called when user types in search bar
+     */
     getSuggestionsAsync(text: string): Promise<string[]>
+    /**
+     * On search
+     */
     searchAsync(text: string): Promise<void>
 
+    /**
+     * On choose tab
+     */
     chooseTabAsync(index: number): Promise<void>
+    /**
+     * On choose search tab
+     */
     chooseSearchTabAsync(index: number): Promise<void>
 
+    /**
+     * On choose (tap/click) group
+     */
     chooseGroupAsync(group: GroupItem): Promise<void>
 
+    /**
+     * Called when showing action of an item
+     */
     buildActionsAsync(indexedItem: IndexedItem, sectionIndex: number): Promise<ItemAction[]>
+    /**
+     * Called when showing actions of multiple items
+     */
     buildMultiActionsAsync(indexedItemsMap: { [key: string]: IndexedItem[] }): Promise<ItemAction[]>
 
+    /**
+     * Called when user taps back button
+     */
     back(): boolean
+    /**
+     * Return value shows whether this source can go back.
+     * If it returns true then app will call [back()] method
+     */
     canBack(): boolean
 
+    /**
+     * Returns bytes to be player by App's player
+     */
     fetchBytesAsync(mi: MusicItem, downloadId: string): Promise<number[]>
+    /**
+     * Returns url to be player by App's player
+     */
     fetchUrlAsync(mi: MusicItem, downloadId: string): Promise<String>
 }
 
@@ -35,6 +78,7 @@ export interface OptionalEventHandlers {
     onOpenedPlaybackPlayPrev(obj: any): void
     onOpenedPlaybackPlayNext(obj: any): void
     onBeforeFetch(obj: any): void
+    onTapArtistTitle(args: any): void
 }
 
 /**
