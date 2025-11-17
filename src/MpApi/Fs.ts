86e6407210a6d031157e76e440ab75a8153a48cb
me <motobep@yandex.ru>
2025-11-17 16:49:47 +0300

    [New version]: 0.3.0

--- src/MpApi/Fs.ts
+++ src/MpApi/Fs.ts
@@ -4,23 +4,23 @@ export declare const sendMessage: SendMessageType
 export class Fs {
     async readFile(path: string,
         options: { encoding: string, flag: string } | string = ''): Promise<string> {
-        return await sendMessage('MpRuntime.fs.readFile',
+        return await sendMessage('MP.runtime.fs.readFile',
             JSON.stringify({ 'path': path, 'options': options }));
     }
 
     existsSync(path: string): boolean {
-        return sendMessage('MpRuntime.fs.existsSync',
+        return sendMessage('MP.runtime.fs.existsSync',
             JSON.stringify({ 'path': path }));
     }
 
     readFileSync(path: string,
         options: { encoding: string, flag: string } | string = ''): string {
-        return sendMessage('MpRuntime.fs.readFileSync',
+        return sendMessage('MP.runtime.fs.readFileSync',
             JSON.stringify({ 'path': path, 'options': options }));
     }
 
     async mpReadAsset(path: string): Promise<string> {
-        return await sendMessage('MpRuntime.fs.mpReadAsset',
+        return await sendMessage('MP.runtime.fs.mpReadAsset',
             JSON.stringify({ 'path': path }));
     }
 }
